65e1eb7e1a318599e5ee2fa92c2328da6610f23e
Nwadike Philip <nwadike@epicdevler.com>
2021-11-23 17:50:23 +0100

    Created InitScreen.kt

--- app/src/main/java/com/epicdevler/training/foodie_ui/startup/InitScreen.kt
+++ app/src/main/java/com/epicdevler/training/foodie_ui/startup/InitScreen.kt
@@ -1,9 +1,12 @@
 package com.epicdevler.training.foodie_ui.startup
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.epicdevler.training.foodie_ui.R
+import com.epicdevler.training.foodie_ui.R.color.orange
 import com.epicdevler.training.foodie_ui.databinding.InitScreenActivityLayoutBinding
 
 @SuppressLint("CustomSplashScreen")
@@ -14,6 +17,10 @@ class InitScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = InitScreenActivityLayoutBinding.inflate(layoutInflater)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.statusBarColor = resources.getColor(orange, null)
+        }
 
         setContentView(binding.root)
 
